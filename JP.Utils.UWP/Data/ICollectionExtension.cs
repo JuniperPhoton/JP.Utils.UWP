f21999f0d7fa946f0bae696161f7a7efe4719a87
juniperphoton <dengweichao@hotmail.com>
2015-12-02 01:30:42 +0800

    Added ICollectionExtension.cs

---
+++ JP.Utils.UWP/Data/ICollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JP.Utils.Data
+{
+    public static class ICollectionExtension
+    {
+        public static ICollection<T> Adding<T>(this ICollection<T> list,T item)
+        {
+            list.Add(item);
+            return list;
+        }
+    }
+}
