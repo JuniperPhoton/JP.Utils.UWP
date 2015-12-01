b9f76fa60020dae1481879bacf27cd8af82b0336
juniperphoton <dengweichao@hotmail.com>
2015-12-02 01:29:46 +0800

    12.2 update

--- JP.Utils.UWP/Device/DeviceHelper.cs
+++ JP.Utils.UWP/Device/DeviceHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Windows.ApplicationModel.Resources.Core;
+using Windows.System.Profile;
 
 namespace JP.Utils.Helper
 {
@@ -25,6 +26,35 @@ namespace JP.Utils.Helper
             {
                 var qualifiers = ResourceContext.GetForCurrentView().QualifierValues;
                 return qualifiers.ContainsKey("DeviceFamily") && qualifiers["DeviceFamily"] == "Mobile";
+            }
+        }
+
+        private static string[] GetDeviceOsVersion()
+        {
+            string sv = AnalyticsInfo.VersionInfo.DeviceFamilyVersion;
+            ulong v = ulong.Parse(sv);
+            ulong v1 = (v & 0xFFFF000000000000L) >> 48;
+            ulong v2 = (v & 0x0000FFFF00000000L) >> 32;
+            ulong v3 = (v & 0x00000000FFFF0000L) >> 16;
+            ulong v4 = (v & 0x000000000000FFFFL);
+            return new string[] { v1.ToString(),v2.ToString(),v3.ToString(),v4.ToString()};
+        }
+
+        public static bool IsTH1OS
+        {
+            get
+            {
+                var versions = GetDeviceOsVersion();
+                return versions[2] == "10240" ? true : false;
+            }
+        }
+
+        public static bool IsTH2OS
+        {
+            get
+            {
+                var versions = GetDeviceOsVersion();
+                return versions[2] == "10586" ? true : false;
             }
         }
     }
