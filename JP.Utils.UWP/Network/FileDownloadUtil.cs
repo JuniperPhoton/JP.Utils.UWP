d45d46dc4f090a8e9c3ebdf3b5f0b0068ae9e816
JuniperPhoton <dengweichao@hotmail.com>
2016-05-17 14:50:08 +0800

    Fix some issues.

---
+++ JP.Utils.UWP/Network/FileDownloadUtil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+
+namespace JP.Utils.Network
+{
+    public static class FileDownloadUtil
+    {
+        /// <summary>
+        /// 根据 URL 获取流
+        /// </summary>
+        /// <param name="url">URL 地址</param>
+        /// <returns>返回IRandomAccessStream</returns>
+        public static async Task<IRandomAccessStream> GetIRandomAccessStreamFromUrlAsync(string url, CancellationToken token)
+        {
+            if (string.IsNullOrEmpty(url)) throw new UriFormatException("The url is null or empty.");
+
+            using (HttpClient client = new HttpClient())
+            {
+                var downloadTask = client.GetAsync(new Uri(url), token);
+
+                token.ThrowIfCancellationRequested();
+
+                var response = await downloadTask;
+                response.EnsureSuccessStatusCode();
+
+                var streamTask = response.Content.ReadAsStreamAsync();
+
+                token.ThrowIfCancellationRequested();
+
+                var stream = await streamTask;
+                
+                return stream.AsRandomAccessStream();
+            }
+        }
+    }
+}
--- JP.Utils.UWP/Network/HttpRequestSender.cs
+++ JP.Utils.UWP/Network/HttpRequestSender.cs
@@ -1,63 +1,31 @@
-﻿using System;
+﻿using JP.API;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using Windows.Data.Json;
-using JP.Utils.Data.Json;
 using Windows.Storage.Streams;
 using Windows.Web.Http;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.IO;
-using JP.Exceptions;
-using System.Threading;
 
-namespace JP.API
+namespace JP.Utils.Network
 {
-    //About to be deprecated.
-    public static class APIHelper
+    public static class HttpRequestSender
     {
-        /// <summary>
-        /// 根据 URL 获取流
-        /// </summary>
-        /// <param name="url">URL 地址</param>
-        /// <returns>返回IRandomAccessStream</returns>
-        public static async Task<IRandomAccessStream> GetIRandomAccessStreamFromUrlAsync(string url)
-        {
-            if (string.IsNullOrEmpty(url)) throw new UriFormatException();
-            using (HttpClient client = new HttpClient())
-            {
-                try
-                {
-                    var response = await client.GetAsync(new Uri(url));
-                    response.EnsureSuccessStatusCode();
-
-                    var buffer = await response.Content.ReadAsBufferAsync();
-                    var streamImage = buffer.AsStream();
-                    
-                    return streamImage.AsRandomAccessStream();
-                }
-                catch(Exception)
-                {
-                    return null;
-                }
-            }
-        }
-
-        /// <summary>
-        /// 发送 Get 请求
         /// </summary>
         /// <typeparam name="T">返回的对象</typeparam>
         /// <param name="url">URL</param>
         /// <returns></returns>
-        public static async Task<CommonRespMsg> SendGetRequestAsync(string url,CancellationToken token)
+        public static async Task<CommonRespMsg> SendGetRequestAsync(string url, CancellationToken token)
         {
             try
             {
                 HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, new Uri(url));
-                return await SendRequest(request,token);
+                return await SendRequest(request, token);
             }
-            catch(TaskCanceledException e)
+            catch (TaskCanceledException e)
             {
                 throw e;
             }
@@ -74,20 +42,20 @@ namespace JP.API
         /// <param name="url">URL</param>
         /// <param name="paras">POST 参数</param>
         /// <returns></returns>
-        public static async Task<CommonRespMsg> SendPostRequestAsync(string url, List<KeyValuePair<string, string>> paras,CancellationToken token)
+        public static async Task<CommonRespMsg> SendPostRequestAsync(string url, List<KeyValuePair<string, string>> paras, CancellationToken token)
         {
             try
             {
                 HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, new Uri(url));
                 request.Content = new HttpFormUrlEncodedContent(paras);
 
-                return await SendRequest(request,token);
+                return await SendRequest(request, token);
             }
-            catch(TaskCanceledException ex)
+            catch (TaskCanceledException ex)
             {
                 throw ex;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return new CommonRespMsg() { IsSuccessful = false, ExtraErrorMsg = ex.Message };
             }
@@ -104,21 +72,21 @@ namespace JP.API
         {
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, new Uri(url));
             request.Content = new HttpStringContent(paramsInRaw);
-            return await SendRequest(request,token);
+            return await SendRequest(request, token);
         }
 
-        public static async Task<CommonRespMsg> SendRequest(HttpRequestMessage request,CancellationToken token)
+        public static async Task<CommonRespMsg> SendRequest(HttpRequestMessage request, CancellationToken token)
         {
             var msgToReturn = new CommonRespMsg();
             try
-            {                
+            {
                 using (var client = new HttpClient())
                 {
                     //client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Linux; U; Android 5.1; zh-cn; XT1085 Build/LPE23.32-53) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
                     HttpResponseMessage resp = null;
-                    if(token== null)
+                    if (token == null)
                     {
-                        resp=await client.SendRequestAsync(request);
+                        resp = await client.SendRequestAsync(request);
                     }
                     else resp = await client.SendRequestAsync(request).AsTask(token);
                     resp.EnsureSuccessStatusCode();
@@ -127,31 +95,14 @@ namespace JP.API
                     var content = Encoding.UTF8.GetString(bytes.ToArray());
                     msgToReturn.JsonSrc = content;
 
-                    JsonObject resultObj;
-                    var ok = JsonObject.TryParse(content, out resultObj);
-                    if (ok)
-                    {
-                        var errorCode = JsonParser.GetStringFromJsonObj(resultObj, "errorCode");
-                        var errorMsg = JsonParser.GetStringFromJsonObj(resultObj, "errorMsg");
-
-                        if (!string.IsNullOrEmpty(errorCode))
-                        {
-                            if (errorCode != "0")
-                            {
-                                msgToReturn.IsSuccessful = false;
-                            }
-                            msgToReturn.ErrorCode = int.Parse(errorCode);
-                            msgToReturn.ErrorMsg = errorMsg;
-                        }
-                    }
                     resp.Dispose();
                 }
             }
-            catch(TaskCanceledException e)
+            catch (TaskCanceledException e)
             {
                 throw e;
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 msgToReturn.IsSuccessful = false;
                 msgToReturn.ExtraErrorMsg += e.Message;
